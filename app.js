f157a26d4608288af8ec25af2d8e38a1c332cbff
jsjlewis96 <jsjlewis96@gmail.com>
2017-10-29 15:17:34 +0000

    removed qunit test routing

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var init_test = require('./routes/init_test');
 
 var app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/init_test', init_test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
