03ea1b23693b88fc85fd35229ca45c26ad179434
jsjlewis96 <jsjlewis96@gmail.com>
2017-11-22 14:52:34 +0000

    fixed hash function on userFactory

--- app/models/userFactory.js
+++ app/models/userFactory.js
@@ -2,18 +2,21 @@ var bcrypt = require('bcrypt-nodejs');
 
 var factory = {
     create: function(username, password) {
-        var user;
-        user.username = username;
-        user.password = password;
+        var user = {
+            username: username,
+            password: bcrypt.hashSync(password, bcrypt.genSaltSync(12), null)
+        };
 
         user.generateHash = function() {
             return bcrypt.hashSync(user.password, bcrypt.genSaltSync(12), null);
         }
 
-        user.validPassword = function() {
+        user.validPassword = function(password) {
             return bcrypt.compareSync(user.password, this.local.password);
         }
 
         return user;
     }
 };
+
+module.exports = factory;
