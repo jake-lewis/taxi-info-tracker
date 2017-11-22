a6f40dba1defed1ae34ef707cec95800e945659a
jsjlewis96 <jsjlewis96@gmail.com>
2017-11-22 14:52:51 +0000

    added login route

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,11 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/',
+        failureRedirect: '/about',
+        failureFlash: true
+    }));
 
     app.get('/signup', function(req, res) {
         // render the page and pass in any flash data if it exists
