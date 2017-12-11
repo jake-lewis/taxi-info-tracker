419c2debd7b97ca09587ba3c418510316a42ec17
jsjlewis96 <jsjlewis96@gmail.com>
2017-12-11 20:14:30 +0000

    added directions srevice

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,8 +1,14 @@
 function initMap() {
     var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService();
     var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: {lat: 41.85, lng: -87.65}
     });
     directionsDisplay.setMap(map);
+    directionsService.route(route, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      });
 };
