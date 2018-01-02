8ed5d72ab846d321e6d41d3c7eb3ae32ebdf0142
jsjlewis96 <jsjlewis96@gmail.com>
2018-01-02 13:48:43 +0000

    added login test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var app = require('../app');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
+var expect = chai.expect;
 var jobFactory = require('../app/models/jobFactory');
 var dbConnection = require('../app/models/dbConnection');
 
@@ -43,6 +44,41 @@ describe("Test Factories", function() {
         //Mocha handles returning a Promise, without needing a failure handler or done() callback
         return dbConnection.getConnection().then((connection) => {
             connection.state.should.equal('authenticated');
+        }).catch((error) => {
+            assert.isNotOk(error, 'Promise error');
         });
     });
+});
+
+describe("Test authentication", function(done) {
+
+    //Increased timeout
+    it("Successful Login", function(done) {
+        chai.request(app)
+            .post('/login')
+            .send({
+                username: 'testUser',
+                password: 'password'
+            })
+            .end(function(err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            });
+    }).timeout(50000);
+
+    it("Failed Login", function(done) {
+
+        chai.request(app)
+            .post('/login')
+            .send({
+                username: 'testUser',
+                password: 'error'
+            })
+            .end(function(err, res) {
+                //Apparently checking against err for "Unauthorized" doesn't work with chai-http ¯\_(ツ)_/¯
+                expect(res).to.have.status(401);
+                done();
+            });
+    }).timeout(50000);
 });
