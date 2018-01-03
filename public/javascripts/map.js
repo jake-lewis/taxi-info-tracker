33721d1a05e2d3a9950fa6a2124878390466bc84
jsjlewis96 <jsjlewis96@gmail.com>
2018-01-03 20:32:34 +0000

    map stuff

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,14 +1,54 @@
 function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService();
-    var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 41.85, lng: -87.65}
-    });
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var plymouth = new google.maps.LatLng(50.3755, -4.1427);
+    var mapOptions = {
+      zoom:13,
+      center: plymouth
+    }
+    
+    infoWindow = new google.maps.InfoWindow;
+    
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        map.setCenter(pos);
+        }, function() {
+        handleLocationError(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false);
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
     directionsDisplay.setMap(map);
-    directionsService.route(route, function(result, status) {
-        if (status == 'OK') {
-          directionsDisplay.setDirections(result);
-        }
-      });
-};
+  }
+  
+  function handleLocationError(browserHasGeolocation) {
+    console.error(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
+
+  function calcRoute() {
+      
+    var directionsService = new google.maps.DirectionsService();
+    var start = document.getElementById('origin').value;
+    var end = document.getElementById('destination').value;
+    var request = {
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function(result, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(result);
+      }
+    });
+  }
